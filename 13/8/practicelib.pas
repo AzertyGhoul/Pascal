0cc37bf31d8bfed7cabadc2003c711514e61e274
AzertyGhoul <93530779+AzertyGhoul@users.noreply.github.com>
2021-11-19 22:29:12 +0500

    Update practicelib.pas

--- 13/8/practicelib.pas
+++ 13/8/practicelib.pas
@@ -2,19 +2,19 @@
 
 
 interface
-  function average(a, b: integer):real;  
-  function averageGeom(a, b: integer):real;
+  function hypotenuse(a, b: integer):real;  
+  function squareTriangle(a, b: integer):real;
   
  
 implementation
-  function average(a, b: integer):real;
+  function hypotenuse(a, b: integer):real;
     var tmp:real;
     begin
       tmp:= sqrt(sqr(a)+sqr(b));
       average := tmp;
     end;
   
-  function averageGeom(a, b: integer):real;
+  function squareTriangle(a, b: integer):real;
   var tmpr:real;
     begin
       tmpr:= (a*b)/2;
